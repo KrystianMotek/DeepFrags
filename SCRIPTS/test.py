57f5c42a4d964d6cec7adb294ad6e431b9deff13
Krystian Motek <101654344+KrystianMotek@users.noreply.github.com>
2022-12-16 00:07:43 +0100

    Code cleanup

--- SCRIPTS/test.py
+++ SCRIPTS/test.py
@@ -24,16 +24,12 @@ if __name__ == "__main__":
     aa = args.aa 
     ss = args.ss 
     model = args.model 
-    model_name = os.path.splitext(os.path.basename(model))[0]
 
-    # generate vector for given sequence and secondary structure
-    label_vector = Label(aa, ss).to_vector()
+    label_vector = Label(aa, ss).to_vector() # generate vector for given sequence and secondary structure
 
-    # number of features in label vector
-    label_dim = len(label_vector)
+    label_dim = len(label_vector) # number of features in label vector
 
-    # define input layer
-    label = tf.keras.layers.Input(shape=(label_dim,))
+    label = tf.keras.layers.Input(shape=(label_dim,)) # define input layer
 
     # latent space processing
     latent_variables = np.load(f"{model}/latent.npy")
@@ -41,19 +37,17 @@ if __name__ == "__main__":
     mean, log_variance = latent_variables[0], latent_variables[1]
     z = np.concatenate([i + np.exp(0.5 * j) * np.random.normal(loc=0.0, scale=1.0, size=(1, latent_dim)) for i, j in zip(mean, log_variance)])
 
-    # random sample from the latent space
-    latent_sample = np.array(random.sample(list(z), 1))
+    latent_sample = np.array(random.sample(list(z), 1)) # random sample from the latent space
 
-    # load decoder and take weights
-    decoder = tf.keras.models.load_model(f"{model}/decoder.h5")
-    decoder.load_weights(f"{model}/weights.h5", by_name=True, skip_mismatch=True)
+    decoder = tf.keras.models.load_model(f"{model}/decoder.h5") # load decoder 
 
-    # predict cooridnates for given label
-    output_vector = decoder.predict(tf.keras.layers.concatenate([latent_sample, label_vector]))[0]
-    output = Output(output_vector).to_original()
+    decoder.load_weights(f"{model}/weights.h5", by_name=True, skip_mismatch=True) # take values of trained weights 
+
+    output_vector = decoder.predict(tf.keras.layers.concatenate([latent_sample, label_vector]))[0] # predict cooridnates for given label
+
+    output = Output(output_vector).to_original() # reverse output to original form
     
-    # compute norm of displacement
-    displacement_norm = np.linalg.norm(output[0:3])
+    displacement_norm = np.linalg.norm(output[0:3]) # compute norm of displacement
 
     # show results
     print(output)
