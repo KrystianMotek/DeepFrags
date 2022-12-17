5b957eac1015513b8aebf3e028e897651a0daa22
Krystian Motek <101654344+KrystianMotek@users.noreply.github.com>
2022-12-18 00:55:40 +0100

    Create scraper.cpp

---
+++ SCRIPTS/scraper.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*
+    script for data generating 
+    extract random fragments from proteins collection
+*/
+
+bool isCorrect(std::string line)
+{
+    // check if given line contains sequence and secondary structure written in a proper way
+    bool correct = true;
+    return correct;
+}
+
+int main()
+{
+    return 0;
+}
