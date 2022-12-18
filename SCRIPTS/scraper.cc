ade5cdf457953324006e2dd7643ce23c6d65bdb6
Krystian Motek <101654344+KrystianMotek@users.noreply.github.com>
2022-12-18 10:31:11 +0100

    Update scraper.cc

--- SCRIPTS/scraper.cc
+++ SCRIPTS/scraper.cc
@@ -1,11 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
 /*
     script for data generating 
     extract random fragments from proteins collection
+    example line shown below
+    1a62A 3 THR 4 MNLTELKNTPV CCHHHHHCCCH 1.284 -5.477 -2.600 99.739 -124.579 91.327 49.439 90.431 53.359 90.948 45.020 87.256 56.852
 */
 
-bool isCorrect(std::string line)
+bool isCorrect(string line)
 {
     // check if given line contains sequence and secondary structure written in a proper way
     bool correct = true;
