71e357bcd5cad53209898f4a0b05aed48b5abe23
Krystian Motek <101654344+KrystianMotek@users.noreply.github.com>
2022-12-25 23:03:45 +0100

    Make import compatible

--- SCRIPTS/test.py
+++ SCRIPTS/test.py
@@ -9,7 +9,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 import numpy as np
 from data.data import Label
-from statistics.utils import Output
+from statistics.statistics import Output
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
