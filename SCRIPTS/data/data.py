8fc4a72b895ba0f2d469a395610eb6893c3a7022
Krystian Motek <101654344+KrystianMotek@users.noreply.github.com>
2022-12-25 22:20:45 +0100

    Main block moved to separate file

--- SCRIPTS/data/data.py
+++ SCRIPTS/data/data.py
@@ -1,4 +1,3 @@
-import argparse
 import warnings
 import os
 
@@ -7,8 +6,6 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 import tensorflow as tf
 import numpy as np
-from statistics.statistics import hec_distribution
-from statistics.plots import alpha_histogram, theta_histogram, correlation_plot
 
 '''
     Example line from data file
@@ -123,52 +120,3 @@ class DataSet:
 
     def save_labels(self, file):
         np.save(file, self.labels_tensor())
-
-
-if __name__ == "__main__":
-    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", type=str, help="path to the data file")
-    args = parser.parse_args()
-    
-    # directory where generated files will be stored
-    work_directory = os.path.dirname(args.file)
-
-    # load and save
-    data = DataSet(args.file)
-    data.save_inputs(f"{work_directory}/inputs")
-    data.save_labels(f"{work_directory}/labels")
-
-    observations = data.observations
-
-    # get statistics of plane and dihedral angles in data set
-    ss_alpha_theta = [[observation.ss(), observation.alpha(), observation.theta()] for observation in observations]
-    angles = np.concatenate([[[ss, float(alpha), float(theta)] for ss, alpha, theta in zip(element[0], element[1][0], element[2][0])] for element in ss_alpha_theta])
-
-    # each list represents different secondary structure
-    h_angles, e_angles, c_angles = hec_distribution(angles)[0], hec_distribution(angles)[1], hec_distribution(angles)[2]
-
-    h_alpha = [float(angle[0]) for angle in h_angles]
-    h_theta = [float(angle[1]) for angle in h_angles]
-
-    e_alpha = [float(angle[0]) for angle in e_angles]
-    e_theta = [float(angle[1]) for angle in e_angles]
-
-    c_alpha = [float(angle[0]) for angle in c_angles]
-    c_theta = [float(angle[1]) for angle in c_angles]
-
-    # place for generated plots
-    os.makedirs(f"{work_directory}/graphs")
-
-    alpha_histogram(h_alpha, f"{work_directory}/graphs//h_alpha")
-    theta_histogram(h_theta, f"{work_directory}/graphs/h_theta")
-
-    alpha_histogram(e_alpha, f"{work_directory}/graphs/e_alpha")
-    theta_histogram(e_theta, f"{work_directory}/graphs/e_theta")
-
-    alpha_histogram(c_alpha, f"{work_directory}/graphs/c_alpha")
-    theta_histogram(c_theta, f"{work_directory}/graphs/c_theta")
-
-    # relationship between alpha and theta got from all observations
-    correlation_plot(np.concatenate([h_alpha, e_alpha, c_alpha]), np.concatenate([h_theta, e_theta, c_theta]), f"{work_directory}/graphs/correlation")
