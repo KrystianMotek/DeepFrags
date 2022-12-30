c1d15f05cf21fe7ac81e9b3c00b6fc55bce3920c
Krystian Motek <101654344+KrystianMotek@users.noreply.github.com>
2022-12-30 21:54:08 +0100

    Get extension and files stored in directory

--- SCRIPTS/data/scraper/src/lib.rs
+++ SCRIPTS/data/scraper/src/lib.rs
@@ -1,14 +1,60 @@
+use std::fs;
+use std::path::Path;
+use std::ffi::OsStr;
 use pyo3::prelude::*;
 
 // some functionalities for data generating
 // extract random fragments from proteins collection
 
+static AA_CODES: &'static [char] = &['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'];
+static SS_CODES: &'static [char] = &['H', 'E', 'C'];
+
 #[pyfunction]
-pub fn check_if_correct(line: str) -> bool
+pub fn check_if_correct(line: &str) -> bool
 {
-    let ok = true;
-    let split = line.split_whitespace();
-    let elements: Vec<&str> = split.collect();
+    let mut ok = true;
+    // split line into vector of strings
+    let elements: Vec<&str> = line.split_whitespace().collect();
+    // get amino acids and secondary structure vectors
+    let aa: Vec<char> = elements[4].chars().collect();
+    let ss: Vec<char> = elements[5].chars().collect();
+    for aa in aa
+    {
+        if !AA_CODES.contains(&aa)
+        {
+            ok = false;
+        }
+    }
+    for ss in ss
+    {
+        if !SS_CODES.contains(&ss)
+        {
+            ok = false;
+        }
+    }
+    ok
+}
+
+#[pyfunction]
+pub fn get_extension(file: &str) -> Option<&str>
+{
+    Path::new(file).extension().and_then(OsStr::to_str)
+}
+
+#[pyfunction]
+pub fn collect_files(directory: &str) -> Vec<String>
+{
+    let all = fs::read_dir(directory).unwrap();
+    let mut files = Vec::new();
+    for item in all
+    {
+        let file = item.unwrap().path().display().to_string();
+        if Some(get_extension(&file)) == Some(Some("dat"))
+        {
+            files.push(file);
+        }
+    }
+    files
 }
 
 #[pymodule]
