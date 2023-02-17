676bac53a3f24cec27f0befb9d71863aecc419ee
Krystian Motek <krystianmotek308@gmail.com>
2023-02-17 21:34:20 +0100

    Add histogram in two dimensions

--- src/core/statistical/src/lib.rs
+++ src/core/statistical/src/lib.rs
@@ -1,10 +1,10 @@
 use pyo3::prelude::*;
-use std::collections::HashMap;
 use structural::{Vec3, sin_cos_to_angle, to_radians, angles_to_cartesian};
 
 #[pyclass]
 pub struct Output
 {
+    // dedicated to raw output processing
     #[pyo3(get, set)]
     vector: Vec<f64>,
     #[pyo3(get, set)]
@@ -12,12 +12,35 @@ pub struct Output
 }
 
 #[pyclass]
-pub struct Histogram
+pub struct Histogram1D
 {
     #[pyo3(get, set)]
     data: Vec<f64>,
     #[pyo3(get, set)]
     bins: i32,
+    #[pyo3(get, set)]
+    min: f64,
+    #[pyo3(get, set)]
+    max: f64,
+}
+
+#[pyclass]
+pub struct Histogram2D
+{
+    #[pyo3(get, set)]
+    x: Vec<f64>,
+    #[pyo3(get, set)]
+    y: Vec<f64>,
+    #[pyo3(get, set)]
+    bins: i32,
+    #[pyo3(get, set)]
+    x_min: f64,
+    #[pyo3(get, set)]
+    x_max: f64,
+    #[pyo3(get, set)]
+    y_min: f64,
+    #[pyo3(get, set)]
+    y_max: f64,
 }
 
 #[pymethods]
@@ -153,39 +176,91 @@ impl Output
 }
 
 #[pymethods]
-impl Histogram
+impl Histogram1D
 {
     #[new]
-    pub fn new(data: Vec<f64>, bins: i32) -> Self
+    pub fn new(data: Vec<f64>, bins: i32, min: f64, max: f64) -> Self
     {
-        Histogram{data, bins}
+        Histogram1D{data, bins, min, max}
     }
 
-    pub fn split(&self) -> HashMap<i32, Vec<f64>>
+    pub fn split(&self) -> Vec<(f64, f64, i32)>
     {
-        let min: f64 = *self.data.iter().min_by(|a, b| a.total_cmp(b)).unwrap();
-        let max: f64 = *self.data.iter().max_by(|a, b| a.total_cmp(b)).unwrap();
-        let pointer: f64 = (max - min) / (self.bins as f64);
-        let mut results = HashMap::new();
-        let mut i: f64 = min;
-        let mut j: i32 = 1;
-        while i < max
+        let pointer: f64 = (self.max - self.min) / (self.bins as f64);
+        let mut results = Vec::new();
+        let mut current_value: f64 = self.min;
+        while current_value < self.max
         {
-            let mut range = Vec::new();
+            let mut n: i32 = 0;
             for element in self.data.iter()
             {
-                if element > &i && element <= &(i + pointer)
+                if element > &current_value && element <= &(current_value + pointer)
                 {
-                    range.push(element.clone());
+                    n += 1;
+                }
+                if current_value == self.min && element == &self.min
+                {
+                    n += 1;
                 }
             }
-            if j == 1
+            results.push((current_value, current_value + pointer, n));
+            current_value += pointer;
+        }
+        results
+    }
+}
+
+#[pymethods]
+impl Histogram2D
+{
+    #[new]
+    pub fn new(x: Vec<f64>, y: Vec<f64>, bins: i32, x_min: f64, x_max: f64, y_min: f64, y_max: f64) -> Self
+    {
+        Histogram2D{x, y, bins, x_min, x_max, y_min, y_max}
+    }
+
+    pub fn split(&self) -> Vec<(f64, f64, f64, f64, i32)>
+    {
+        let x_pointer: f64 = (self.x_max - self.x_min) / (self.bins as f64);
+        let y_pointer: f64 = (self.y_max - self.y_min) / (self.bins as f64);
+        let mut results = Vec::new();
+        let mut current_x: f64 = self.x_min;
+        while current_x < self.x_max 
+        {
+            let mut current_y: f64 = self.y_min;
+            while current_y < self.y_max
             {
-                range.push(min);
+                let mut n: i32 = 0;
+                for x in self.x.iter()
+                {
+                    let index = self.x.iter().position(|&k| k == *x).unwrap();
+                    if x > &current_x && x <= &(current_x + x_pointer)
+                    {
+                        if self.y[index] > current_y && self.y[index] <= (current_y + y_pointer)
+                        {
+                            n += 1;
+                        }
+                        if current_y == self.y_min && self.y[index] == self.y_min
+                        {
+                            n += 1;
+                        }
+                    }
+                    if current_x == self.x_min && x == &self.x_min
+                    {
+                        if self.y[index] > current_y && self.y[index] <= (current_y + y_pointer)
+                        {
+                            n += 1;
+                        }
+                        if current_y == self.y_min && self.y[index] == self.y_min
+                        {
+                            n += 1;
+                        }
+                    }
+                }
+                results.push((current_x, current_x + x_pointer, current_y, current_y + y_pointer, n));
+                current_y += y_pointer;
             }
-            results.insert(j, range);
-            j += 1;
-            i += pointer;
+            current_x += x_pointer;
         }
         results
     }
@@ -195,6 +270,7 @@ impl Histogram
 fn statistical(_: Python, m: &PyModule) -> PyResult<()>
 {
     m.add_class::<Output>()?;
-    m.add_class::<Histogram>()?;
+    m.add_class::<Histogram1D>()?;
+    m.add_class::<Histogram2D>()?;
     Ok(())
 }
