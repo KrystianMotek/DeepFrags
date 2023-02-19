34bf8566fd82082ed592a540508be07df56d92ae
Krystian Motek <krystianmotek308@gmail.com>
2023-02-19 10:08:23 +0100

    New methods in insertion

--- src/core/insertion/src/lib.rs
+++ src/core/insertion/src/lib.rs
@@ -2,6 +2,7 @@ use pyo3::prelude::*;
 use structural::{Vec3};
 
 #[pyclass]
+#[derive(Clone)]
 pub struct Fragment
 {
     #[pyo3(get, set)]
@@ -15,6 +16,27 @@ impl Fragment
     pub fn new(atoms: Vec<Vec3>) -> Self
     {
         Fragment{atoms}
+    }
+
+    pub fn to_local(&mut self)
+    {
+        let n: usize = self.atoms.len();
+        for i in 0..(n-1)
+        {
+            let start = self.atoms[0].clone();
+            self.atoms[i].subtract(&start);
+        }
+    }
+
+    pub fn spanning_vector(&self) -> Vec3
+    {
+        // from the first atom to the last one
+        let n: usize = self.atoms.len();
+        let start = self.atoms[0].clone();
+        let mut end = self.atoms[n-1].clone();
+        end.subtract(&start);
+        let vector = end.clone();
+        vector
     }
 }
 
