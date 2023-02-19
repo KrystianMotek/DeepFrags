f657efed5bba294663757bcb19c7be411c46f718
Krystian Motek <krystianmotek308@gmail.com>
2023-02-19 16:15:12 +0100

    Move insertion tools to structural

--- src/core/structural/src/lib.rs
+++ src/core/structural/src/lib.rs
@@ -15,6 +15,14 @@ pub struct Vec3
     pub z: f64,
 }
 
+#[pyclass]
+#[derive(Clone)]
+pub struct Fragment
+{
+    #[pyo3(get, set)]
+    pub atoms: Vec<Vec3>,
+}
+
 #[pymethods]
 impl Vec3
 {
@@ -62,6 +70,37 @@ impl Vec3
         self.x -= vector.x;
         self.y -= vector.y;
         self.z -= vector.z;
+    }
+}
+
+#[pymethods]
+impl Fragment
+{
+    #[new]
+    pub fn new(atoms: Vec<Vec3>) -> Self
+    {
+        Fragment{atoms}
+    }
+
+    pub fn to_local(&mut self)
+    {
+        let n: usize = self.atoms.len();
+        for i in 0..(n-1)
+        {
+            let start = self.atoms[0].clone();
+            self.atoms[i].subtract(&start);
+        }
+    }
+
+    pub fn spanning_vector(&self) -> Vec3
+    {
+        // from the first atom to the last one
+        let n: usize = self.atoms.len();
+        let start = self.atoms[0].clone();
+        let mut end = self.atoms[n-1].clone();
+        end.subtract(&start);
+        let vector = end.clone();
+        vector
     }
 }
 
@@ -242,6 +281,7 @@ pub fn angles_to_cartesian(atom_1: &mut Vec3, atom_2: &mut Vec3, atom_3: &mut Ve
 fn structural(_: Python, m: &PyModule) -> PyResult<()>
 {
     m.add_class::<Vec3>()?;
+    m.add_class::<Fragment>()?;
     m.add_function(wrap_pyfunction!(to_degrees, m)?).unwrap();
     m.add_function(wrap_pyfunction!(to_radians, m)?).unwrap();  
     m.add_function(wrap_pyfunction!(dot_product, m)?).unwrap();
