8fd68b7274e624f8e588bf8a798d191fcb2a912d
Krystian Motek <krystianmotek308@gmail.com>
2023-03-04 09:01:46 +0100

    PDB tools development

--- src/core/proteins.py
+++ src/core/proteins.py
@@ -16,19 +16,47 @@ class Atom:
         self.y = self.coordinates.y
         self.z = self.coordinates.z
 
-    def is_carbon_alpha(self):
+    def element(self):
+        if self.name in ["NA", "K", "MG", "ZN", "CU", "FE", "SE", "CO", "MN", "MO"]:
+            return self.name 
+        else:
+            return self.name[0]
+
+    def is_ca(self):
         return self.name == "CA"
 
 
 class Residue:
-    def __init__(self, atoms: List[Atom]):
+    def __init__(self, id, name, atoms: List[Atom]):
+        self.id = id 
+        self.name = name
         self.atoms = atoms
+    
+    def translate_three_letters(self):
+        return RESIDUES[self.name]
 
 
 class Chain:
     def __init__(self, name, residues: List[Residue]):
         self.name = name
         self.residues = residues
+
+    def sequence(self, i, j):
+        string = ""
+        for residue in self.residues[i-1:j-1]:
+            string += residue.translate_three_letters()
+        return string
+    
+    def extract_ca(self):
+        trace = []
+        for residue in self.residues:
+            for atom in residue:
+                if atom.is_ca():
+                    trace.append(atom)
+        return trace
+
+    def displacement(self):
+        pass
 
 
 class Structure:
@@ -67,3 +95,8 @@ class LineParser:
     
     def load_atom(self):
         return Atom(id=self.read_id(), name=self.read_name(), coordinates=Vec3(x=self.read_x(), y=self.read_y(), z=self.read_z()))
+    
+
+class FileParser:
+    def __init__(self, file):
+        self.file = file
