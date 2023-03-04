0bd4551b19c1fdac3064d7753e7539df42c5be72
Krystian Motek <krystianmotek308@gmail.com>
2023-03-04 14:53:20 +0100

    PDB tools development

--- src/core/proteins.py
+++ src/core/proteins.py
@@ -3,100 +3,86 @@ from utils.structural import Vec3, two_atoms_vector
 
 # tools for reading PDB files
 
-RESIDUES = {"ALA": "A", "ARG": "R", "ASN": "N", "ASP": "D", "CYS": "C", "GLN": "Q", "GLU": "E", "GLY": "G", "HIS": "H", "ILE": "I", "LEU": "L", "LYS": "K", "MET": "M", "PHE": "F", "PRO": "P", "SER": "S", "THR": "T", "TRP": "W", "TYR": "Y", "VAL": "V"}
+RESIDUES = {"ALA": "A", "ARG": "R", "ASN": "N", "ASP": "D", "CYS": "C", "GLN": "Q", "GLU": "E", "GLY": "G", "HIS": "H", "ILE": "I",
+            "LEU": "L", "LYS": "K", "MET": "M", "PHE": "F", "PRO": "P", "SER": "S", "THR": "T", "TRP": "W", "TYR": "Y", "VAL": "V"}
 
 
-class Atom:
-    def __init__(self, id, name, coordinates: Vec3):
-        self.id = id 
-        self.name = name 
-        self.coordinates = coordinates
+class CarbonAlpha:
+    def __init__(self, ss, id, residue, chain_name, coordinates: Vec3):
+        self._ss = ss 
+        self._id = id 
+        self._residue = residue
+        self._chain_name = chain_name
+        self._coordinates = coordinates
 
-        self.x = self.coordinates.x
-        self.y = self.coordinates.y
-        self.z = self.coordinates.z
+    def __str__(self):
+        pass
 
-    def element(self):
-        if self.name in ["NA", "K", "MG", "ZN", "CU", "FE", "SE", "CO", "MN", "MO"]:
-            return self.name 
-        else:
-            return self.name[0]
+    @property
+    def ss(self):
+        return self._ss 
 
-    def is_ca(self):
-        return self.name == "CA"
+    @property
+    def id(self):
+        return self._id 
 
-
-class Residue:
-    def __init__(self, id, name, atoms: List[Atom]):
-        self.id = id 
-        self.name = name
-        self.atoms = atoms
+    @property
+    def residue(self):
+        return self._residue
     
-    def translate_three_letters(self):
-        return RESIDUES[self.name]
+    @property
+    def chain_name(self):
+        return self._chain_name
+    
+    @property
+    def coordinates(self):
+        return self._coordinates
+    
+    @coordinates.setter
+    def coordinates(self, vector: Vec3):
+        self._coordinates = vector
 
 
 class Chain:
-    def __init__(self, name, residues: List[Residue]):
-        self.name = name
-        self.residues = residues
+    def __init__(self, atoms: List[CarbonAlpha]):
+        self._atoms = atoms 
+    
+    @property
+    def atoms(self):
+        return self._atoms
 
     def sequence(self, i, j):
         string = ""
-        for residue in self.residues[i-1:j-1]:
-            string += residue.translate_three_letters()
+        atoms = self.atoms()[i-1:j-1]
+        for atom in atoms:
+            string += RESIDUES[atom.residue()]
+        return string 
+    
+    def secondary_structure(self, i, j):
+        string = ""
+        atoms = self.atoms()[i-1:j-1]
+        for atom in atoms:
+            string += atom.ss()
         return string
     
-    def extract_ca(self):
-        trace = []
-        for residue in self.residues:
-            for atom in residue:
-                if atom.is_ca():
-                    trace.append(atom)
-        return trace
-
-    def displacement(self):
-        pass
+    def displacement(self, i, j):
+        coordinates_i = self.atoms[i-1].coordinates()
+        coordinates_j = self.atoms[j-1].coordinates()
+        vector = two_atoms_vector(coordinates_i, coordinates_j)
+        return vector.length()
 
 
 class Structure:
     def __init__(self, chains: List[Chain]):
-        self.chain = chains
+        self._chains = chains
 
-
-class LineParser:
-    def __init__(self, line: str):
-        self.line = line
+    @property
+    def chains(self):
+        return self._chains
     
-    def read_id(self):
-        START = 6
-        END = 11
-        return int(self.line[START:END].strip())
-
-    def read_name(self):
-        START = 12
-        END = 16
-        return self.line[START:END].strip()
-    
-    def read_x(self):
-        START = 30
-        END = 38
-        return float(self.line[START:END].strip())
-    
-    def read_y(self):
-        START = 38
-        END = 47
-        return float(self.line[START:END].strip())
-    
-    def read_z(self):
-        START = 47
-        END = 54
-        return float(self.line[START:END].strip())
-    
-    def load_atom(self):
-        return Atom(id=self.read_id(), name=self.read_name(), coordinates=Vec3(x=self.read_x(), y=self.read_y(), z=self.read_z()))
-    
-
-class FileParser:
-    def __init__(self, file):
-        self.file = file
+    def to_pdb(self):
+        lines = []
+        for chain in self.chains():
+            for atom in chain.atoms():
+                lines.append(atom.__str__())
+        return lines
