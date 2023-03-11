84e2eef7ef15a01389e168a66f4b3603c5e25e65
Krystian Motek <krystianmotek308@gmail.com>
2023-03-11 21:16:58 +0100

    Angles statistics app development

--- src/apps/calculate_statistics.py
+++ src/apps/calculate_statistics.py
@@ -14,14 +14,16 @@ if __name__ == "__main__":
     
     labels = np.load(args.labels)
 
-    ss = []
-    for label in labels:
-        ss.append(LabelMLP.extract_ss(label))
+    n = (np.shape(labels) - 3) / 23 
 
     model = args.model 
     decoder = DecoderLoader(decoder=f"{model}/decoder.pb", latent=f"{model}/latent.npy")
 
     reconstructed_data = decoder.predict(labels)
+
+    ss = []
+    for label in labels:
+        ss.append(LabelMLP.extract_ss(label))
 
     alpha = []
     theta = []
@@ -29,6 +31,18 @@ if __name__ == "__main__":
         output = Output(vector=vector)
         alpha.append(output.alpha())
         theta.append(output.theta())
+
+    h_angles = []
+    e_angles = []
+    c_angles = []
+    for ss, alpha, theta in zip(ss, alpha, theta):
+        for i in range(n):
+            if ss[i] == "H":
+                h_angles.append([alpha[i], theta[i]])
+            if ss[i] == "E":
+                e_angles.append([alpha[i], theta[i]])
+            if ss[i] == "C":
+                c_angles.append([alpha[i], theta[i]])
 
     # planar and dihedral angles correlation
     # histograms for each secondary structure
